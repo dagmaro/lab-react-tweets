dad889ee1fa0c795842f8b7e4b879d9c8c3caed5
Dagmara Grabowska <dagmarag0@gmail.com>
2023-02-27 17:53:57 +0100

    done

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,29 @@
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import Timestamp from "./Timestamp";
+import User from "./User";
+
+function Tweet(props) {
+  const { tweet } = props;
+  return (
+    <div className="tweet">
+      <ProfileImage image={tweet.user.image} />
+
+      <div className="body">
+        <div className="top">
+          <User user={tweet.user} />
+          <Timestamp time={tweet.timestamp} />
+        </div>
+
+        <Message message={tweet.message} />
+
+        <Actions />
+      </div>
+
+      <i className="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
